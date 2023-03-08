647e6f57e3dc7baef21db77bb40ac9eba93bf138
mangermc <mangermc@mail.uc.edu>
2023-03-08 13:11:13 -0500

    First

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Hide/reveal nav on scroll
+var prevScrollpos = window.pageYOffset; 
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0";
+    } else {
+        document.getElementById("nav").style.top = "-116px";
+    }
+    prevScrollpos = currentScrollPos;
+}
